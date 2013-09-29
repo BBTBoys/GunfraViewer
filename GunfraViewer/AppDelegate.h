bfd7f5d95b78d48255ea9f489c1fd975af569cc9
김두형 <dduskim@daumcorp.com>
2013-09-30 00:51:55 +0900

    Initial Commit

---
+++ GunfraViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GunfraViewer
+//
+//  Created by DdusKim on 13. 9. 30..
+//  Copyright (c) 2013년 DdusKim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GunfraViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GunfraViewer
+//
+//  Created by DdusKim on 13. 9. 30..
+//  Copyright (c) 2013년 DdusKim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
